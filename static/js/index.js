4f85825988fee3fc7960c1dd45c908cb518870b2
JAEMIN <a41213908@gmail.com>
2021-09-26 20:48:53 +0900

    입력값 date 추가 및 기타 수정

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,38 @@
+window.onload = function (){
+    main();
+}
+
+function main(){
+    showClock();
+}
+
+
 function showClock(){
     var currentDate = new Date();
     var divClock = document.getElementById('time')
     divClock.innerHTML = currentDate.getHours().toString().padStart(2, '0') + ":" + currentDate.getMinutes().toString().padStart(2, '0');
     setInterval("showClock()", 1000);  // 1초마다 실행
 }
+
+function getTodo(){
+    var todoJson = new Object();
+    var currentDate = new Date();
+
+    todoJson.name = "재민";
+    todoJson.date = currentDate.getFullYear().toString() + "-" + currentDate.getMonth().toString().padStart(2, '0') + "-" + currentDate.getDate().toString().padStart(2, '0');
+    todoJson.todo = document.getElementById('todo-input').value;
+
+    var today_json = JSON.stringify(todoJson);
+
+    console.log(today_json);
+
+    const fs = require('fs');
+    fs.writeFileSync
+}
+
+function entercheck(){
+    if(window.event.keyCode == 13){
+        getTodo();
+    }
+}
+
